af98ce6d252c6abb1b09073580b9edb2d469d6d3
Hannah Luebbering <61786322+hluebbering@users.noreply.github.com>
2022-03-08 04:23:37 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-# initializes the project after it has been cloned
+# Initializes project after it has been cloned + describes all of the metadata about project
+# Required fields: name, version, and packages
+
+from distutils.core import setup
+
+setup(
+  name = 'GravitationalWaves', # Must be unique if published to PyPI
+  version = '0.1dev', # Keeps track of different project releases
+  packages = ['towelstuff',], # Describes where the Python source code is within project
+  license = 'Creative Commons Attribution-Noncommercial-Share Alike license', # Includes information about the license 
+  long_description = open('README.md').read(), # Re-uses README file for long_description field
+)
