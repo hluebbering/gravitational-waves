5d0c0c52d61bb2bfbb52d566296639b4665fc251
Hannah Luebbering <61786322+hluebbering@users.noreply.github.com>
2022-03-08 04:25:03 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 from distutils.core import setup
 
 setup(
-  name = 'GravitationalWaves', # Must be unique if published to PyPI
+  name = 'gravitational-waves', # Must be unique if published to PyPI
   version = '0.1dev', # Keeps track of different project releases
-  packages = ['towelstuff',], # Describes where the Python source code is within project
+  packages = ['gravitational-waves',], # Describes where the Python source code is within project
   license = 'Creative Commons Attribution-Noncommercial-Share Alike license', # Includes information about the license 
   long_description = open('README.md').read(), # Re-uses README file for long_description field
 )
